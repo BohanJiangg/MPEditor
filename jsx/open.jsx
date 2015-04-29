ba50d938f1c14b2b4d1076349e52cd9d775a8e08
Ectoplasma34 <bohandongqi@yahoo.com>
2015-04-28 21:35:44 -0400

    MPEditor

---
+++ jsx/open.jsx
@@ -0,0 +1,9 @@
+﻿ var myFile = File.openDialog("Please select file to open...");
+  if (myFile != null){
+  // open file
+  open(myFile);
+ 
+
+  }else{
+    alert("No file selected.");
+  }
---
+++ jsx/save.jsx
@@ -0,0 +1,7 @@
+﻿#target photoshop
+var  destDir = Folder.selectDialog( 'Select the export directory.');
+		
+var jpegOptions = new JPEGSaveOptions();
+		jpegOptions.quality = 10;
+		jpegOptions.embedColorProfile = false;
+		app.activeDocument.saveAs( File( destDir+ "/" + activeDocument.name+ ".jpg"), jpegOptions, false);
